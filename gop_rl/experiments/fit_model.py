dc3a794d6c83f3462c703496d29d3fe901cc7ff3
mdecke <mdecke@student.ethz.ch>
2025-04-22 15:41:22 -0700

    train and plot function for expert model

---
+++ gop_rl/experiments/fit_model.py
@@ -0,0 +1,53 @@
+import os
+import argparse
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import ast
+
+from gop_rl.modeling import mle, cnf
+
+def main():
+    parser = argparse.ArgumentParser(description="train expert model with expert data.")
+    parser.add_argument("--env_name", type=str, default="Pendulum-v1", help="Name of the environment.")
+    parser.add_argument("--model_type", type=str, default="mle", help="Type of model to train.")
+    parser.add_argument("--input_type", type=str, default="state", help="Type of input for the model.")
+    parser.add_argument("--epochs", type=int, default=100, help="of gradient steps.")
+    parser.add_argument("--batch_size", type=int, default=64, help="Batch size for training.")
+    parser.add_argument("--early_stopping", type=int, default=10, help="How long we look for over fitting.")
+    parser.add_argument("--data_dir", type=str, default="data/pendulum", help="Directory to save expert data.")
+    parser.add_argument("--output_dir", type=str, default="outputs/pendulum", help="Directory to save plots.")
+    parser.add_argument("--action_limit", type=float, default=2.0, help="Action limit for the environment.")
+    parser.add_argument("--standardize", type=bool, default=True, help="Z-score standardization of model inputs.")
+    parser.add_argument("--seed", type=int, default=0, help="Random seed for reproducibility.")
+    parser.add_argument("--device", type=str, default="cpu", help="Device to use for training.")
+    args = parser.parse_args()
+
+    np.random.seed(args.seed)
+
+    os.makedirs(args.output_dir, exist_ok=True)
+    os.makedirs(args.data_dir, exist_ok=True)
+
+    if args.env_name == 'Pendulum-v1':
+        csv_file_name = f'data/pendulum/elqr_5000_episodes.csv'
+    elif args.env_name == 'Ant-v4':
+        csv_file_name = f'data/ant/ppo_250_episodes.csv'
+    elif args.env_name == 'Walker2d-v4':
+        csv_file_name = f'data/walker/ppo_250_episodes.csv'
+    else:  
+        raise ValueError("Invalid environment name. Must be 'Pendulum-v1', 'Ant-v4' or 'Walker2d-v4'")
+    
+
+    if args.model_type == 'mle':
+        _,history = mle.train_model(args,csv_file_name)
+        mle.plot_metrics(history, f'{args.output_dir}/{args.input_type}_mle_metrics.png')
+    elif args.model_type == 'cnf':
+        cnf.train_model(args,csv_file_name)
+    else:
+        raise ValueError("Invalid model type. Must be 'mle' or 'cnf'")
+    
+    print('[INFO] Model trained and saved.')
+    
+   
+if __name__ == "__main__":
+    main()
