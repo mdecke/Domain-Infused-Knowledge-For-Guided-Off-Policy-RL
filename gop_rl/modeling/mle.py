e0d5811a68cddc564fc1ecd0e4bb9a94161e57d6
mdecke <mdecke@student.ethz.ch>
2025-04-24 08:48:58 -0700

    put early stoping in init function for both

--- gop_rl/modeling/mle.py
+++ gop_rl/modeling/mle.py
@@ -11,35 +11,10 @@ import pandas as pd
 import gymnasium as gym
 
 from gop_rl.utils import data_processor, prepare_data
+from gop_rl.modeling import EarlyStopping
 
 
 _LOG_2PI_HALF = 0.5*torch.log(torch.tensor(2) * torch.pi)
-
-class EarlyStopping:
-    def __init__(self, patience=7, min_delta=0, path='best_model.pth'):
-        self.patience = patience
-        self.min_delta = min_delta
-        self.path = path
-        self.counter = 0
-        self.best_loss = None
-        self.early_stop = False
-        
-    def __call__(self, val_loss, model):
-        if self.best_loss is None:
-            self.best_loss = val_loss
-            self.save_checkpoint(model)
-        elif val_loss > self.best_loss + self.min_delta:
-            self.counter += 1
-            if self.counter >= self.patience:
-                self.early_stop = True
-        else:
-            self.best_loss = val_loss
-            self.save_checkpoint(model)
-            self.counter = 0
-            
-    def save_checkpoint(self, model):
-        torch.save(model.state_dict(), self.path)
-
 
 class ActionMLE(nn.Module):
     def __init__(self, state_dim, action_dim, action_lim):
