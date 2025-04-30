ee6be335c59ca6f5db6d34ca456c83f6663e10a4
mdecke <mdecke@student.ethz.ch>
2025-04-30 10:37:02 -0700

    hyperparams

--- gop_rl/experiments/run_model_fitting.sh
+++ gop_rl/experiments/run_model_fitting.sh
@@ -5,12 +5,12 @@ ENV_NAME="Ant-v4"
 DATA_DIR="data/ant"
 BASE_OUT="outputs/ant"
 
-MODEL_TYPES=('cnf')
+MODEL_TYPES=(mle cnf)
 INPUT_TYPES=('state' 'state_action' 'prev_state_action' 'state_prev_state')
 
 for MODEL in "${MODEL_TYPES[@]}"; do
   for INPUT in "${INPUT_TYPES[@]}"; do
-    OUT_DIR="${BASE_OUT}/${MODEL}/${INPUT}"
+    OUT_DIR="${BASE_OUT}"
     mkdir -p "${OUT_DIR}"
 
     echo "▶ ${ENV_NAME} | model=${MODEL} | input=${INPUT}"
@@ -22,7 +22,8 @@ for MODEL in "${MODEL_TYPES[@]}"; do
         --output_dir   "${OUT_DIR}" \
         --epoch 100 \
         --batch_size 2048 \
-        --n_flows 2 \
+        --early_stopping 5 \
+        --n_flows 12 \
         --n_cycles 1\
         --seed 42
   done
@@ -33,34 +34,36 @@ echo "✓ All Ant experiments completed."
 
 
 
-# echo "✓ Starting Pendulum experiments."
+echo "✓ Starting Pendulum experiments."
 
-# ENV_NAME="Pendulum-v1"
-# DATA_DIR="data/pendulum"
-# BASE_OUT="outputs/pendulum"
+ENV_NAME="Pendulum-v1"
+DATA_DIR="data/pendulum"
+BASE_OUT="outputs/pendulum"
 
-# MODEL_TYPES=('mle' 'cnf')
-# INPUT_TYPES=('state' 'state_action' 'prev_state_action' 'state_prev_state')
+MODEL_TYPES=('mle' 'cnf')
+INPUT_TYPES=('state' 'state_action' 'prev_state_action' 'state_prev_state')
 
-# for MODEL in "${MODEL_TYPES[@]}"; do
-#   for INPUT in "${INPUT_TYPES[@]}"; do
-#     OUT_DIR="${BASE_OUT}/${MODEL}/${INPUT}"
-#     mkdir -p "${OUT_DIR}"
+for MODEL in "${MODEL_TYPES[@]}"; do
+  for INPUT in "${INPUT_TYPES[@]}"; do
+    OUT_DIR="${BASE_OUT}"
+    mkdir -p "${OUT_DIR}"
 
-#     echo "▶ ${ENV_NAME} | model=${MODEL} | input=${INPUT}"
-#     python3 -m gop_rl.experiments.fit_model \
-#         --env_name     "${ENV_NAME}" \
-#         --model_type   "${MODEL}" \
-#         --input_type   "${INPUT}" \
-#         --data_dir     "${DATA_DIR}" \
-#         --output_dir   "${OUT_DIR}" \
-#         --nb_traj 50\
-#         --n_cycles 1\
-#         --early_stopping 10\
-#         --epochs 100\
-#         --batch_size 128\
-#         --n_flows 6
-#   done
-# done
+    echo "▶ ${ENV_NAME} | model=${MODEL} | input=${INPUT}"
+    python3 -m gop_rl.experiments.fit_model \
+        --env_name     "${ENV_NAME}" \
+        --model_type   "${MODEL}" \
+        --input_type   "${INPUT}" \
+        --data_dir     "${DATA_DIR}" \
+        --output_dir   "${OUT_DIR}" \
+        --nb_traj 32\
+        --n_grad_steps 20\
+        --n_cycles 1\
+        --early_stopping 4\
+        --epochs 100\
+        --batch_size 256\
+        --n_flows 6\
+        --seed 42
+  done
+done
 
-# echo "✓ All Pendulum experiments completed."
+echo "✓ All Pendulum experiments completed."
